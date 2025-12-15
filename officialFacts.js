86a02fb316b57a13f89754795802d08e65102272
jhfgiiikjhi-sudo <jhfgiiikjhi@gmail.com>
2025-12-15 13:15:02 +0700

    Update officialFacts.js

---
+++ officialFacts.js
@@ -0,0 +1,10 @@
+module.exports = {
+  primeMinister: "เศรษฐา ทวีสิน",
+  capital: "กรุงเทพมหานคร",
+  currency: "บาท",
+  emergency: {
+    police: "191",
+    fire: "199",
+    ambulance: "1669",
+  },
+};
